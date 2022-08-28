8e0ce74a8eb9f2405a0cc5ff12abf7d3c48e3f82
nihadae <alakbarzadenihad@gmail.com>
2022-08-28 23:53:47 +0400

    Added.

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+  Particles.init({
+    selector: '.background',
+    color: ['#fff','#fcd15c'],
+    sizeVariations: 10
+  });
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 4000);
+};
+
+function openNav(){
+  document.getElementById("sideNav").style.width = "35%";
+  document.getElementById("sideNav").style.right = "0%";
+  document.getElementById("sideNav").style.transition = "0.3s ease-in-out";
+}
+
+function closeNav(){
+  document.getElementById("sideNav").style.width = "0%";
+  document.getElementById("sideNav").style.right = "-100%"
+}
+
